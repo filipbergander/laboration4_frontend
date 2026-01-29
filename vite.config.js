d15e0c6a1ab1d78da4922eac0f16ac28b629e48c
Filip <filip.bergander1996@gmail.com>
2026-01-29 15:20:56 +0100

    Skapat grund och struktur för sidor inom webbplatsen. Lagt till node_modules i .gitignore också vidare.

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                sass: resolve(__dirname, "sass.html"),
+                animation: resolve(__dirname, "animation.html"),
+                diagram: resolve(__dirname, "diagram.html"),
+                map: resolve(__dirname, "map.html")
+            }
+        }
+    }
+});
