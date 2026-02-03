053c0d664dbc4b7561c062cb90d99fa14185baba
Filip <filip.bergander1996@gmail.com>
2026-02-03 19:35:13 +0100

    Grid-layout på bilderna för en bättre responsiv design.

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ const darkBtn = document.querySelector(".btn-darkened"); // Knapp för att toggl
 darkBtn.addEventListener("click", () => {
     document.body.classList.toggle("dark"); // Lägger till classlist dark på body.
     darkBtn.classList.toggle("dark"); // Lägger till classlist dark på knappen
-    if (bodyDark) {
+    if (document.body.classList.contains("dark")) { // Om body.dark finns ändras knappens text
         darkBtn.innerHTML = "Ljust tema"; // Ändrar texten inom knappen
     } else {
         darkBtn.innerHTML = "Mörkt tema";
